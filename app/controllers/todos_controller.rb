96fbbbb180e850a4ee82523cb8a2275d322eedf0
swojeet <swojeetkayastha@gmail.com>
2019-08-05 12:26:33 +0545

    Allow a user to create a Todo

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,25 @@
 class TodosController < ApplicationController
   def index
-    
+    @todos = Todo.all
   end
+
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = Todo.create(todos_params)
+    if @todo.valid?
+      redirect_to todos_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def todos_params
+    params.require(:todo).permit(:title)
+  end
+
 end
