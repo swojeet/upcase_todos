f9b115e58626a9bbd362d3cc80a976baa8169de3
swojeet <swojeetkayastha@gmail.com>
2019-08-05 17:30:06 +0545

    Refactor tests to use helper methods

--- spec/features/user_completes_todo_spec.rb
+++ spec/features/user_completes_todo_spec.rb
@@ -4,12 +4,11 @@ feature "User completes a todo" do
   scenario "successfully" do
     sign_in
 
-    click_on "Add a new Todo"
-    fill_in "Title", with: "Buy milk"
-    click_on "Submit"
+    create_todo "Buy milk"
 
     # check ('Buy milk')
     click_on "Mark as complete"
-    expect(page).to have_css '.todos li.completed', text: "Buy milk"
+    expect(page).to display_completed_todo "Buy milk"
   end
+
 end
